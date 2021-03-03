2c650869da3832f503c31936ae0ea941d541e02b
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2021-03-03 10:47:36 +0700

    Percobaan Pertama

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,24 +1,32 @@
 import React from 'react'
-import {Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core';
-import {AddShoppingCart} from '@material-ui/icons'
-import classes from '*.module.css';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
 
-const Product = () => {
+import useStyles from './styles';
+
+const Product = ({ product }) => {
+    const classes = useStyles();
     return (
         <Card className = {classes.root}>
-            <CardMedia className={classes.media} image = '' title={Product.name}/>
+            <CardMedia className={classes.media} image = '' title={product.name}/>
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant="h5" gutterBottom>
-                        {Product.name}
+                        {product.name}
                     </Typography>
-                    <Typography variant="h5" gutterBottom>
-                        {Product.name}
+                    <Typography variant="h5">
+                        {product.price}
                     </Typography>
                 </div>
+                <Typography variant="h2" color="textSecondary">{product.description}</Typography>
             </CardContent>
+            <CardActions disableSpacing className={classes.CardActions}>
+                <IconButton aria-label = "Add to Cart">
+                    <AddShoppingCart/>
+                </IconButton>
+            </CardActions>
         </Card>
     )
 }
 
-export default Product
+export default Product;
